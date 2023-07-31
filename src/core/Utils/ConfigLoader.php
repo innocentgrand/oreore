6e452faa52ea193d4b8cf9c383c1e1b1dc256863
yuta <yamakawa@innocentgrand.com>
2023-07-31 09:38:12 +0900

    Baseになるコンフィグを追加

---
+++ src/core/Utils/ConfigLoader.php
@@ -0,0 +1,46 @@
+<?php
+namespace Ore\Utils;
+use Exception;
+
+class ConfigLoader 
+{
+	private $_configDir = "";
+
+	public function __construct($base)
+	{
+		$this->_configDir = $base . "/config/";
+		if (!is_dir($this->_configDir)) 
+		{
+			throw new Exception("log dir NOT FOUNT.");
+		}
+	}
+
+	public function load($name)
+	{
+		if (file_exists($this->_configDir.$name))
+		{
+			$jsonStr = file_get_contents($this->_configDir.$name);
+			try {
+				var_dump($jsonStr);
+				$arr = json_decode($jsonStr, true);
+				var_dump($arr);
+				if ($arr) 
+				{
+					return $arr;
+				}
+				else 
+				{
+					throw new Exception("JSON ERROR!");
+				}
+			}
+			catch(Exception $e) {
+				throw $e;
+			}
+			catch(ValueError $v) {
+				throw $v;
+			}
+		}
+	}
+
+
+}
--- src/core/boot.php
+++ src/core/boot.php
@@ -1,6 +1,25 @@
 <?php
-namespace Ore;
+//namespace Ore;
 use Ore\Controller\Ctrl;
+use Ore\Utils\ConfigLoader;
 
 require_once __DIR__  . "/../../vendor/autoload.php";
+
+$config = new ConfigLoader(dirname(dirname(__DIR__)));
+$configBase = $config->load("base.json");
+
+$debug = false;
+
+if ($configBase['php']['debug'])
+{
+	$debug = true;
+}
+
+if ($debug)
+{
+	$whoops = new \Whoops\Run;
+	$whoops ->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+	$whoops->register();
+}
+
 
